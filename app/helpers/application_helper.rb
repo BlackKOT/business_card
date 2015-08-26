8eddb950c10216c2a420ffc0a6423dc34aacaf1d
Oleg Kotenko <blackkot@ukr.net>
2015-08-26 17:53:39 +0300

    added flags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
   def lang_option(lang_name)
-    (link_to lang_name, root_path(locale: lang_name), class: "#{'selected' if I18n.locale == lang_name.to_sym}")
+    link_to(root_path(locale: lang_name)) do
+      content_tag(:div, '&nbsp;', class: "flag #{lang_name.to_sym}_flag #{'selected' if I18n.locale == lang_name.to_sym}")
+    end
   end
 end
