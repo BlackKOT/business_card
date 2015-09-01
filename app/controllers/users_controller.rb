a8c54adf38eb8aecf5bd6c19a32dfb0b1ad97502
Oleg Kotenko <blackkot@ukr.net>
2015-09-01 23:35:48 +0300

    moved redirect to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @agent = request.user_agent.downcase
+    delim = if @agent.index('iphone') || @agent.index('ipad')
+      iOSVersion = ((@agent.match(/\b[0-9]+_[0-9]+(?:_[0-9]+)?\b/))[0].split('_')[0]).to_i
+      iOSVersion < 8 ?  ';' : '&'
+    else
+      '?'
+    end
+    redirect_to "sms: 907654736#{delim}body=this is a body text"
   end
 
   def show
