def7083a6aa9ced51a22274f628bf4ee0b49dd5b
Jenua Boiko <jeyboy1985@gmail.com>
2015-09-13 19:34:45 +0300

    fix for index directive

--- app/assets/javascripts/angular/admin.js
+++ app/assets/javascripts/angular/admin.js
@@ -3,11 +3,5 @@
 //= require_directory ./admin
 
 window.appModule = angular.module('admin', []);
-
-appModule.config([
-    '$httpProvider', function($httpProvider) {
-        $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content')
-    }]);
-
 appModule.run(function() { console.log('angular app running') });
 
--- app/assets/javascripts/angular/application.js
+++ app/assets/javascripts/angular/application.js
@@ -2,16 +2,6 @@
 //= require_directory ./shared
 //= require_directory ./application
 
-appModule = angular.module('application', []);
-
-appModule.config([
-    '$httpProvider', function($httpProvider) {
-        $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content')
-        //$httpProvider.defaults.useXDomain = true;
-        //delete $httpProvider.defaults.headers.common["X-Requested-With"];
-        //$httpProvider.defaults.headers.common["Accept"] = "application/json";
-        //$httpProvider.defaults.headers.common["Content-Type"] = "application/json";
-    }]);
-
+window.appModule = angular.module('application', []);
 appModule.run(function() { console.log('angular app running for application') });
 
