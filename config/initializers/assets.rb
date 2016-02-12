06130d060720774d4a412e8357821031af011685
Jenua Boiko <jeyboy@bigmir.net>
2016-02-12 16:41:22 +0200

    fixes for layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,5 @@ Rails.application.assets.register_engine('.slim', Slim::Template)
 Slim::Engine.set_options(
     attr_list_delims: {'(' => ')', '[' => ']'},
     code_attr_delims: {'(' => ')', '[' => ']'},
-    format: :html5
+    format: :html
 )
