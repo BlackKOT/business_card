99a78eeb738fa9df6cb051dd97e6566f7fb8cbd2
Jenua Boiko <jeyboy@bigmir.net>
2016-02-12 15:58:23 +0200

    fix for change language links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,18 @@
 module ApplicationHelper
   def lang_option(lang_name)
-    link_to(root_path(locale: lang_name)) do
+    exp = /\/en\/|\/ru\//
+
+    lang_url = if (request.path =~ exp)
+      request.path.gsub(exp, "/#{lang_name}/")
+    else
+      uri = URI.parse(request.path)
+      uri.query = Rack::Utils.parse_nested_query(uri.query).merge('locale' =>  lang_name).to_query
+      uri.to_s
+    end
+
+
+
+    link_to(lang_url) do
       content_tag(:div, '&nbsp;', class: "flag #{lang_name.to_sym}_flag #{'selected' if I18n.locale == lang_name.to_sym}")
     end
   end
